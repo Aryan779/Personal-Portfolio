3792202e5995aa436cfb974f04a08fcae08e7a03
Aryan Patel <84985729+Aryan779@users.noreply.github.com>
2022-10-19 21:13:27 +0530

    Add files via upload

---
+++ Aryan's website/script.js
@@ -0,0 +1,15 @@
+console.log('script running...');
+document.querySelector('#cross').style.display = 'none';
+document.querySelector('.ham').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo'))
+    {
+        document.querySelector('#bar').style.display='inline';
+        document.querySelector('#cross').style.display='none';
+    }
+    else{
+        document.querySelector('#bar').style.display='none';
+        setTimeout(()=>{document.querySelector('#cross').style.display='inline';},350);
+    }
+});
+
